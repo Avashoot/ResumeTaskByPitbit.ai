199418531be4abc6a93a6f67b1bfc4a7b508ef21
Avadhoot Sutar <avadhootsutar2103@gmail.com>
2024-08-02 00:56:50 +0530

    data.js and index.js is added

---
+++ data.js
@@ -0,0 +1,105 @@
+// data.js
+
+const profile = {
+    name: "Avadhoot Sutar",
+    contact: {
+      email: "avadhootsutar2103@gmail.com",
+      github: "https://github.com/Avashoot",
+      linkedin: "https://www.linkedin.com/in/avadhoot-sutar-883377228/"
+    },
+    education: [
+      {
+        institution: "Vishwakarma Institute of Technology, Pune",
+        degree: "Artificial Intelligence and Data Science",
+        graduation_date: "June 2025",
+        current_cgpa: "8.56/10.00"
+      },
+      {
+        institution: "MAH-HSC",
+        graduation_date: "June 2021",
+        score: "94.33"
+      },
+      {
+        institution: "MAH-SSC",
+        graduation_date: "June 2019",
+        score: "93.20"
+      }
+    ],
+    coursework: [
+      "Object-Oriented Programming",
+      "Data Structures & Algorithms",
+      "Discrete Math",
+      "Linear Algebra",
+      "Calculus",
+      "Data Science",
+      "Machine Learning",
+      "Computer Network",
+      "Computer Architecture",
+      "Probability & Statistics"
+    ],
+    skills: {
+      languages: ["C", "Python", "Java", "JavaScript", "HTML/CSS"],
+      tools: ["Git/GitHub", "VS Code", "IntelliJ IDEA", "Notepad", "Arduino"]
+    },
+    projects: [
+      {
+        name: "Enhanced Predictive Modeling in R: Data Sampling & Features",
+        technologies: ["R", "VS Code"],
+        date: "May 2023",
+        description: [
+          "Developed a machine learning model to predict heart attack likelihood using data from 700+ individuals with 8 parameters, achieving an 84% accuracy rate.",
+          "Selected a suitable algorithm, split the data for training/testing, and optimized the model, improving performance by 5%."
+        ]
+      },
+      {
+        name: "Insights Unveiled: Data Analysis & Sentiment Analysis Platform",
+        technologies: ["Python", "NumPy", "Pandas", "Streamlit"],
+        date: "Feb 2024",
+        description: [
+          "Analyzed 27,000 rows of data, including up to 1,500 bad reviews, to identify trends and patterns in Mama Earth Company’s sales data for 2022-23.",
+          "Developed interactive data visualization tools using Plotly and Streamlit libraries, resulting in improved user engagement and understanding of complex sales data.",
+          "Utilized NLP to categorize sentiments in customer reviews, enabling targeted improvement strategies."
+        ]
+      },
+      {
+        name: "Connect Four",
+        technologies: ["C", "C++"],
+        date: "Nov 2023",
+        description: [
+          "Drop discs in columns, update the grid, and check for winning connections.",
+          "Simple yet strategic, accessible to all ages, fostering critical thinking."
+        ]
+      },
+      {
+        name: "Shortest Path Finder using A* algorithm",
+        technologies: ["Java"],
+        date: "Feb 2024",
+        description: [
+          "Experienced in developing and applying algorithms, such as A*, to solve intricate problems effectively.",
+          "Proficient in analyzing and refining algorithms for better performance, improving application functionality and user interaction."
+        ]
+      }
+    ],
+    experience: [
+      {
+        organization: "Compute Society of India Club",
+        role: "Multimedia Coordinator",
+        duration: "Aug 2022 - July 2023"
+      }
+    ],
+    achievements: [
+      {
+        title: "Rank 2 in Tensor FIESTA Hackathon",
+        date: "Feb 2024",
+        organization: "Google Developer Student Clubs - VIT, Pune"
+      }
+    ],
+    hobbies: [
+      {
+        activity: "Sketching Pictures",
+        year_started: 2018,
+        note: "Intermediate Grade Examination"
+      }
+    ]
+  };
+  
